b7e097e5a398de45317ed29d388a779975b24c37
EzBlox <84008951+EzBlox@users.noreply.github.com>
2021-11-14 22:49:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
-$keys = array('youmaypass',); 
-$users = array('3037561072',); 
-$sub = $_GET["key"];
-$subuser = $_GET["user"];
-if (in_array($sub,$keys,TRUE)) if (in_array($subuser,$users,TRUE)) {
-print('True');
+
